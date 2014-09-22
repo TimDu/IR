41373a878aa04c94eac91249fddce3fbafbd67ac
James Clay <jc2005grad@yahoo.com>
2014-09-22 14:23:57 -0400

    Added stemmer logic and updated stop words rule test.

--- newsindexer/edu/buffalo/cse/irf14/analysis/StemmerRule.java
+++ newsindexer/edu/buffalo/cse/irf14/analysis/StemmerRule.java
@@ -1,22 +1,51 @@
 package edu.buffalo.cse.irf14.analysis;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.Vector;
+
 public class StemmerRule extends TokenFilter {
-	
-	
+
 	public StemmerRule(TokenStream stream) {
 		super(stream);
 	}
-	
+
 	@Override
 	public boolean increment() throws TokenizerException {
-		// TODO Auto-generated method stub
-		return false;
+		Token tok = stream.next();
+
+		// Begin error checking
+		if (tok == null) {
+			throw new TokenizerException();
+		}
+		String term = tok.toString();
+		if (term == null) {
+			throw new TokenizerException();
+		}
+		// End error checking
+
+		// Stem logic
+		Stemmer stem = new Stemmer();
+
+		for (char c : term.toLowerCase().toCharArray()) {
+			if(!Character.isAlphabetic(c))
+			{
+				return stream.hasNext();
+			}
+			stem.add(c);
+			
+		}
+		
+		
+		stem.stem();
+		tok.setTermText(stem.toString());
+		return stream.hasNext();
 	}
 
 	@Override
 	public TokenStream getStream() {
 		// TODO Auto-generated method stub
-		return null;
+		return stream;
 	}
 
 }
--- newsindexer/edu/buffalo/cse/irf14/analysis/StopWordRule.java
+++ newsindexer/edu/buffalo/cse/irf14/analysis/StopWordRule.java
@@ -1,17 +1,43 @@
 package edu.buffalo.cse.irf14.analysis;
 
+import java.util.Hashtable;
+
+// Hashtable Implementation came from the following 
+// http://javarevisited.blogspot.com/2012/01/java-hashtable-example-tutorial-code.html#ixzz3E4GcNAc8
 public class StopWordRule extends TokenFilter {
-	
-	final String []stopWordList = {"do", "not", "this", "is", "a", "of"};
-	
+
+	final String[] stopWordList = { "a", "able", "about", "across", "after",
+			"all", "almost", "also", "am", "among", "an", "and", "any", "are",
+			"as", "at", "be", "because", "been", "but", "by", "can", "cannot",
+			"could", "dear", "did", "do", "does", "either", "else", "ever",
+			"every", "for", "from", "get", "got", "had", "has", "have", "he",
+			"her", "hers", "him", "his", "how", "however", "i", "if", "in",
+			"into", "is", "it", "its", "just", "least", "let", "like",
+			"likely", "may", "me", "might", "most", "must", "my", "neither",
+			"no", "nor", "not", "of", "off", "often", "on", "only", "or",
+			"other", "our", "own", "rather", "said", "say", "says", "she",
+			"should", "since", "so", "some", "than", "that", "the", "their",
+			"them", "then", "there", "these", "they", "this", "tis", "to",
+			"too", "twas", "us", "wants", "was", "we", "were", "what", "when",
+			"where", "which", "while", "who", "whom", "why", "will", "with",
+			"would", "yet", "you", "your" };
+
+	private Hashtable<String, String> stopWordHash;
+
 	public StopWordRule(TokenStream stream) {
 		super(stream);
+		stopWordHash = new Hashtable<String, String>();
+		for (String word : stopWordList) {
+			stopWordHash.put(word, word);
+		}
+
 	}
+
 	@Override
 	public boolean increment() throws TokenizerException {
-		
+
 		Token tok = stream.next();
-		
+
 		// Begin error checking
 		if (tok == null) {
 			throw new TokenizerException();
@@ -21,14 +47,12 @@ public class StopWordRule extends TokenFilter {
 			throw new TokenizerException();
 		}
 		// End error checking
-		
-		for (String mark: stopWordList) {
-			if (term.equals(mark))
-			{
-				stream.remove();
-				break;
-			}
+
+		// use hash map for constant Big-Oh lookups
+		if (stopWordHash.containsKey(term)) {
+			stream.remove();
 		}
+
 		return stream.hasNext();
 	}
 

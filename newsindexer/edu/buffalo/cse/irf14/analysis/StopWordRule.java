3d001a4e12412d4cc8ab8dec3730b2f3474397e8
James Clay <jc2005grad@yahoo.com>
2014-09-22 13:10:21 -0400

    Added basic stopword implementation. Waiting on TA for Stemmer implementation.

--- newsindexer/edu/buffalo/cse/irf14/analysis/StopWordRule.java
+++ newsindexer/edu/buffalo/cse/irf14/analysis/StopWordRule.java
@@ -1,20 +1,40 @@
 package edu.buffalo.cse.irf14.analysis;
 
 public class StopWordRule extends TokenFilter {
-
+	
+	final String []stopWordList = {"do", "not", "this", "is", "a", "of"};
+	
 	public StopWordRule(TokenStream stream) {
 		super(stream);
 	}
 	@Override
 	public boolean increment() throws TokenizerException {
-		// TODO Auto-generated method stub
-		return false;
+		
+		Token tok = stream.next();
+		
+		// Begin error checking
+		if (tok == null) {
+			throw new TokenizerException();
+		}
+		String term = tok.toString();
+		if (term == null) {
+			throw new TokenizerException();
+		}
+		// End error checking
+		
+		for (String mark: stopWordList) {
+			if (term.equals(mark))
+			{
+				stream.remove();
+				break;
+			}
+		}
+		return stream.hasNext();
 	}
 
 	@Override
 	public TokenStream getStream() {
-		// TODO Auto-generated method stub
-		return null;
+		return stream;
 	}
 
 }
